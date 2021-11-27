de093afe659d7c9e26a4106f0dcc051dc6752aa4
grigoriy_klimenko <gklimenko>
2021-11-27 15:24:45 +0300

    add API structure

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ const PORT = process.env.PORT || 5000;
 const reqHendler = require("./controller");
 
 const server = http.createServer(async (req, res) => {
+    //GET all persons
     if (req.url === "/person" && req.method === "GET") {
         res.writeHead(200, { "Content-Type": "application/json" });
         const data = await new reqHendler().getAllPersons();
         res.end(JSON.stringify(data));
     }
+    
+    //GET person by id
+    else if (req.url.match(/\/person\/\/([0-9]+)/) && req.method === "GET") {
+
+    }
+
+    //DELETE person
+    else if (req.url.match(/\/person\/([0-9]+)/) && req.method === "DELETE") {
+
+    }
+
+    // PUT - update person
+    else if (req.url.match(/\/person\/([0-9]+)/) && req.method === "PUT") {
+
+    }
+
+    // POST - add person
+    else if (req.url === "/person" && req.method === "POST") {
+
+    }
+
 });
 
 server.listen(PORT, () => {
