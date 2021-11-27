fbe9468e417154ceb69f81f3b74cc4d22de7f1e1
grigoriy_klimenko <gklimenko>
2021-11-27 13:22:53 +0300

    add dotenv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("http");
-const PORT = 5000;
+require('dotenv').config({ path: './src/.env' })
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(async (req, res) => {
     if (req.url === "/person" && req.method === "GET") {
@@ -9,4 +10,6 @@ const server = http.createServer(async (req, res) => {
     }
 });
 
-server.listen(PORT, () => {});
+server.listen(PORT, () => {
+    console.log(`Server port ${PORT}`);
+});
